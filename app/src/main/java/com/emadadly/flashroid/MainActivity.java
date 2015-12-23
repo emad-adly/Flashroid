0cc5ceff22449710573f4577a750322b625f53e3
Emad Adly <emad.adly792@gmail.com>
2015-12-24 00:38:56 +0200

    add ui

--- app/src/main/java/com/emadadly/flashroid/MainActivity.java
+++ app/src/main/java/com/emadadly/flashroid/MainActivity.java
@@ -1,14 +1,17 @@
 package com.emadadly.flashroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    ImageView flashImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flashImageView = (ImageView) findViewById(R.id.flashIV);
 
     }
 }
